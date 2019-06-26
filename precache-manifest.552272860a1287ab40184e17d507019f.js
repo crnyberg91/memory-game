2060051e1ed9f36da39d76aed75c48a3ab255372
Chris Nyberg <chrisn83@gmail.com>
2019-06-26 14:03:51 -0400

    Updates

---
+++ precache-manifest.552272860a1287ab40184e17d507019f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefcb21faf473c0ed0678edbc2ccfd0e",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "5fd1150d95940c5dc82a",
+    "url": "/memory-game/static/css/main.54bbab8f.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/memory-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "5fd1150d95940c5dc82a",
+    "url": "/memory-game/static/js/main.d3aa1d6a.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
